0ba2df8013704f998fda90fdcd6b7b008a1a969d
neerajy818285 <neeraj818285@gmail.com>
2024-03-11 15:04:24 +0530

    second commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="py-3 fixed-bottom  footer my-1">
+        <p className="text-center text-body-secondary">
+          ©Create by <span className="Name">Neeraj</span>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
